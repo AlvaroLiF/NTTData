cbed7b8406d61b15fbc079b5fcb266b0d794c7cb
AlvaroLiF <93470137+AlvaroLiF@users.noreply.github.com>
2022-11-10 22:57:22 +0100

    Taller Práctico 1 - Hibernate

---
+++ nttdatacenters-hibernate-t1-ALF/nttdatacenters-hibernate-t1-ALF/src/main/java/com/nttdata/nttdatacenters_hibernate_t1_alf/main/NTTDataHibernateUtil.java
@@ -0,0 +1,53 @@
+package com.nttdata.nttdatacenters_hibernate_t1_alf.main;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Taller Práctico 1 - Hibernate
+ * 
+ * Clase de configuración
+ * 
+ * @author ALF
+ *
+ */
+public class NTTDataHibernateUtil {
+
+	/** LOGGER */
+	static final Logger log = LoggerFactory.getLogger(NTTDataHibernateUtil.class);
+
+	/** Factoría de sesiones */
+	private static final SessionFactory SESSION_FACTORY;
+
+	/** Constructor privado */
+	private NTTDataHibernateUtil() {
+
+	}
+
+	static {
+
+		try {
+
+			// Generación de la configuración
+			SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
+
+		} catch (final Exception ex) {
+
+			// Error de inicialización
+			log.error(String.format("[ERROR] Configuración de Hibernate - %s", ex));
+			throw new ExceptionInInitializerError();
+		}
+
+	}
+
+	/**
+	 * Devuelve la factoría de sesiones
+	 * 
+	 * @return SessionFactory
+	 */
+	public static SessionFactory getSessionFactory() {
+		return SESSION_FACTORY;
+	}
+}
---
+++ nttdatacenters-hibernate-t1-ALF/nttdatacenters-hibernate-t1-ALF/src/main/java/com/nttdata/nttdatacenters_hibernate_t1_alf/persistence/ClientDaoInterface.java
@@ -0,0 +1,43 @@
+package com.nttdata.nttdatacenters_hibernate_t1_alf.persistence;
+
+import java.util.List;
+
+/**
+ * Taller Práctico 1 - Hibernate
+ * 
+ * DAO de tabla NTTDATA_CLIENT
+ * 
+ * @author ALF
+ *
+ */
+public interface ClientDaoInterface extends CommonDaoInterface<Client> {
+
+	/**
+	 * Obtiene clientes por nombre y número de documento de identidad
+	 * 
+	 * @param firstName
+	 * @param identityCardNumber
+	 * @return List<Client>
+	 */
+	public List<Client> searchByNameAndIdentityCardNumber(final String firstName, final Integer identityCardNumber);
+
+	/**
+	 * Obtiene clientes por nombre
+	 * 
+	 * @param firstName
+	 * @return List<Client>
+	 */
+	public List<Client> searchByFirstName(final String firstName);
+
+	/**
+	 * Obtiene clientes por nombre y apellidos
+	 * 
+	 * @param firstName
+	 * @param firstSurname
+	 * @param secondSurname
+	 * @return List<Client>
+	 */
+	public List<Client> searchByFirstNameAndSurnames(final String firstName, final String firstSurname,
+			final String secondSurname);
+
+}
